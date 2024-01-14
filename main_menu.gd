1265f1b1f098324874a40d187e48ffd3cc53baee
Antonio Dell'Annunziata <contact@antonio-da.dev>
2024-01-14 15:42:04 +0100

    Use test level layout

--- main_menu.gd
+++ main_menu.gd
@@ -10,7 +10,7 @@ func _ready():
 	exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://test_level.tscn")
 
 func _on_settings_button_pressed():
 	pass
