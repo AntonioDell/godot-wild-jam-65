f5c688f1a0633b359d4d7d020c8d30dab78e4b5d
Antonio Dell'Annunziata <contact@antonio-da.dev>
2024-01-15 22:49:26 +0100

    Add game start animation draft

--- main_menu.gd
+++ main_menu.gd
@@ -1,19 +1,14 @@
 extends Node
 
 @onready var start_button = %StartButton
-@onready var settings_button = %SettingsButton
-@onready var exit_button = %ExitButton
+@onready var animation_player = %AnimationPlayer
 
 func _ready():
+	start_button.show()
 	start_button.pressed.connect(_on_start_button_pressed)
-	settings_button.pressed.connect(_on_settings_button_pressed)
-	exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_button_pressed():
+	start_button.hide()
+	animation_player.play("start_game")
+	await animation_player.animation_finished
 	get_tree().change_scene_to_file("res://test_level.tscn")
-
-func _on_settings_button_pressed():
-	pass
-
-func _on_exit_button_pressed():
-	pass
